e9a534ab871d71637d626c0e91841c2d92d92814
keson1 <694314541@qq.com>
2018-03-19 17:26:53 +0800

    This is my first commit via Git!

---
+++ manage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+from app import create_app, db
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+#from flask_bootstrap import Bootstrap
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+#bootstrap = Bootstrap(app)
+
+
+def make_shell_context():
+    return dict(app=app, db=db)
+
+manager.add_command("shell",Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
+
