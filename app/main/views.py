46a3df88ea3c449189037e73291c95adc1b5d15c
keson1 <694314541@qq.com>
2018-03-21 16:25:06 +0800

    修改views.py,增加多线程

--- app/main/views.py
+++ app/main/views.py
@@ -9,6 +9,9 @@ import sys,os
 from . import main
 import yaml
 from paramiko_ssh import ssh
+import threading
+import time
+import Queue
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -17,6 +20,29 @@ os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.AL32UTF8'
 config_file = open(os.path.dirname(os.path.abspath(__file__)) + '/config.yaml')
 info = yaml.safe_load(config_file)
 config_file.close()
+back_info = []
+
+def func(queue, password, port, cmdlist):
+    thread = threading.current_thread()
+    thread_name = thread.getName()
+    while 1:
+        if queue.empty():
+            break
+        else:
+            ip = queue.get()
+            a = ssh(ip, password, port, cmdlist)
+            a.append(ip)
+            a.append(thread_name)
+            back_info.append(a)
+
+
+def huoquxinxi(city):
+    name = info[city]['name']
+    message_ip = info[city]['message_ip']
+    password = info[city]['ssh_password']
+    port = info[city]['ssh_port']
+    ips = info[city]['ips']
+    return name,message_ip,password,port,ips
 
 @main.route('/')
 def index():
@@ -26,10 +52,7 @@ def index():
 
 @main.route('/<city>/redis/<redis_port>/<redis_key>/<auth>/')
 def redis(city, redis_port, redis_key, auth):
-    name = info[city]['name']
-    message_ip = info[city]['message_ip']
-    password = info[city]['ssh_password']
-    port = info[city]['ssh_port']
+    name, message_ip, password, port, ips = huoquxinxi(city)
     cmdlist = []
     if auth == 0:
         cmd = "/yisa_oe/redis/redis-cli -p " + redis_port + " llen " + redis_key
@@ -39,40 +62,40 @@ def redis(city, redis_port, redis_key, auth):
     redis_info = ssh(message_ip, password, port, cmdlist)
     return render_template('redis.html', result=redis_info, city=name, ip=message_ip)
 
-
-@main.route('/<city>/system_info/')
-def system_info(city):
-    name = info[city]['name']
-    ips = info[city]['ips']
-    password = info[city]['ssh_password']
-    port = info[city]['ssh_port']
-    cmdlist = ['df -h|awk \'{print $2,$5,$6}\'|grep -vE \'(/dev|/run|/sys|Use|/boot)\'',
-               'free -h | sed -n 2p| awk \'{print  $2,$7}\'', 'vmstat| sed -n 3p| awk \'{x=100-$15} END {print x}\'',
-               'uptime|grep load|awk -F: \'{print $NF}\'', 'hostname']
-    sys_info = []
+@main.route('/<city>/info/<arg>/')
+def chaxun_info(city, arg):
+    threads = []
+    queue = Queue.Queue(maxsize=20)
+    name, message_ip, password, port, ips = huoquxinxi(city)
     for ip in ips:
-        a = ssh(ip, password, port, cmdlist)
-        a.append(ip)
-        sys_info.append(a)
-    return render_template('system_info.html', info=sys_info, city=name)
-
-@main.route('/<city>/device_info/')
-def device_info(city):
-    name = info[city]['name']
-    ips = info[city]['ips']
-    password = info[city]['ssh_password']
-    port = info[city]['ssh_port']
-    device_info = []
-    cmdlist = ['dmidecode | grep  -A2 \'System Information\'|sed -n \'2,3p\'', #主板
-               'dmidecode -t 4 |grep Version | uniq',                         #cpu
-               'dmidecode -t 17 | grep -e \'Manufacturer\|Size\' | grep -v \'NO DIMM\|No Module Installed\'', #内存
-               '/usr/bin/lsusb |grep \'Aladdin Knowledge Systems HASP\' |wc -l',   #加密狗
-               'fdisk -l | grep -e \'/dev/s.*:\|/dev/s.*：\'|awk -F\' \' \'{print $2 $3 $4}\'',  #硬盘大小
-               'lspci | grep VGA | grep NVIDIA | awk -F[ \'{print $2}\' | awk -F] \'{print $1}\'', #显卡
-               'cat /proc/scsi/scsi | grep \'Model:\'',           #硬盘厂商
-               'hostname']    #主机名
-    for ip in ips:
-        a = ssh(ip, password, port, cmdlist)
-        a.append(ip)
-        device_info.append(a)
-    return render_template('device_info.html', info=device_info, city=name)
+        queue.put(ip)
+    if arg == 'sys':
+        cmdlist = ['df -h|awk \'{print $2,$5,$6}\'|grep -vE \'(/dev|/run|/sys|Use|/boot)\'',
+                   'free -h | sed -n 2p| awk \'{print  $2,$7}\'', 'vmstat| sed -n 3p| awk \'{x=100-$15} END {print x}\'',
+                   'uptime|grep load|awk -F: \'{print $NF}\'', 'hostname']
+    else:
+        cmdlist = ['dmidecode | grep  -A2 \'System Information\'|sed -n \'2,3p\'',  # 主板
+                   'dmidecode -t 4 |grep Version | uniq',  # cpu
+                   'dmidecode -t 17 | grep -e \'Manufacturer\|Size\' | grep -v \'NO DIMM\|No Module Installed\'',  # 内存
+                   '/usr/bin/lsusb |grep \'Aladdin Knowledge Systems HASP\' |wc -l',  # 加密狗
+                   'fdisk -l | grep -e \'/dev/s.*:\|/dev/s.*：\'|awk -F\' \' \'{print $2 $3 $4}\'',  # 硬盘大小
+                   'lspci | grep VGA | grep NVIDIA | awk -F[ \'{print $2}\' | awk -F] \'{print $1}\'',  # 显卡
+                   'cat /proc/scsi/scsi | grep \'Model:\'',  # 硬盘厂商
+                   'hostname']  # 主机名
+    for x in xrange(3):
+        t = threading.Thread(target=func, args=(queue, password, port, cmdlist))
+        threads.append(t)
+    for t in threads:
+        t.start()
+        time.sleep(1)
+    for t in threads:
+        t.join()
+    global back_info
+    tmp_info = back_info
+    back_info = []
+    if arg == 'sys':
+        return render_template('system_info.html', info=tmp_info, city=name)
+    elif arg == 'dev':
+        return render_template('device_info.html', info=tmp_info, city=name)
+    else:
+        return render_template('404.html')
